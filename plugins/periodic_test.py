5986c7aa1dd45895d85cbfaa2648d809c99a36a7
skoczen <skoczen@gmail.com>
2013-12-13 15:53:36 -0800

    fixes hours on test random plugin

--- plugins/periodic_test.py
+++ plugins/periodic_test.py
@@ -6,6 +6,6 @@ from will import settings
 
 class RandomTestPlugin(WillPlugin):
 
-    @randomly(start_hour=13, end_hour=15, num_times_per_day=110, day_of_week="fri")
+    @randomly(start_hour=15, end_hour=18, num_times_per_day=110, day_of_week="fri")
     def random_test(self):
         print "yo" 
