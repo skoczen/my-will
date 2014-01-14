820b6d47d6f1b3a3a9a82c2e9d438068b8a8b5b5
skoczen <skoczen@gmail.com>
2014-01-15 00:39:48 +0100

    Teaches @gk-will  simple @will time

---
+++ plugins/time_topic.py
@@ -0,0 +1,23 @@
+import datetime
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template
+
+
+class TimeTopicPlugin(WillPlugin):
+
+    # Disabled for now.
+    # @periodic(minute='0')
+    def set_topic_time(self):
+        now_pst = datetime.datetime.now()
+        now_bcn = now_pst + datetime.timedelta(hours=9)
+
+        topic = "PST: %s, BCN: %s" % (now_pst.strftime("%a %I:%M %p"), now_bcn.strftime("%a %I:%M %p"))
+        self.set_topic(topic)
+
+    @respond_to("^time$")
+    def tell_times(self, message):
+        now_pst = datetime.datetime.now()
+        now_bcn = now_pst + datetime.timedelta(hours=9)
+ 
+        topic = "PST: %s, BCN: %s" % (now_pst.strftime("%a %I:%M %p"), now_bcn.strftime("%a %I:%M %p"))
+        self.reply(message, topic)
