1e83398f4d844e942c06938ef3de49af0249b87e
skoczen <skoczen@gmail.com>
2014-01-28 10:03:18 +0100

    Teaches will about the marketing site

---
+++ plugins/server/marketing.py
@@ -0,0 +1,13 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template
+
+class MarketingPlugin(WillPlugin):
+
+    @route("/marketing-deployed/<success_code>")
+    def deploy_output(self, success_code):
+        if success_code == 0:
+            self.say("Marketing site deployed successfully: http://gk-marketing.s3-website-us-east-1.amazonaws.com/")
+        else:
+            self.say("Error deploying marketing site.")
+        return "Success"
+
