3368858a9102ff57726a607ca43e63aee5b1eb70
skoczen <skoczen@gmail.com>
2014-01-28 10:26:04 +0100

    Success code is a string.

--- plugins/server/marketing.py
+++ plugins/server/marketing.py
@@ -5,7 +5,7 @@ class MarketingPlugin(WillPlugin):
 
     @route("/marketing-deployed/<success_code>")
     def deploy_output(self, success_code):
-        if success_code == 0:
+        if success_code == "0":
             self.say("Marketing site deployed successfully: http://gk-marketing.s3-website-us-east-1.amazonaws.com/")
         else:
             self.say("Error deploying marketing site.")
