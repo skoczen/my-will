eb69bc2508d7a01f0352746431a4966657852f67
skoczen <skoczen@gmail.com>
2014-02-03 15:23:11 +0100

    Disables walkmaster for now.

--- plugins/walkmaster.py
+++ plugins/walkmaster.py
@@ -4,10 +4,12 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 
 class WalkmasterPlugin(WillPlugin):
 
-    @randomly(start_hour='10', end_hour='17', day_of_week="mon-fri", num_times_per_day=1)
-    def walkmaster(self):
-        now = datetime.datetime.now()
-        in_5_minutes = now + datetime.timedelta(minutes=5)
+    # Disabled for now, since we don't listen to will.
+    pass
+    # @randomly(start_hour='10', end_hour='17', day_of_week="mon-fri", num_times_per_day=1)
+    # def walkmaster(self):
+    #     now = datetime.datetime.now()
+    #     in_5_minutes = now + datetime.timedelta(minutes=5)
 
-        self.say("@all Walk happening in 5 minutes!")
-        self.schedule_say("@all It's walk time!", in_5_minutes)
+    #     self.say("@all Walk happening in 5 minutes!")
+    #     self.schedule_say("@all It's walk time!", in_5_minutes)
