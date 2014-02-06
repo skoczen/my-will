2c783da408c0db1fba477181774d4d62bf490af0
skoczen <skoczen@gmail.com>
2014-02-06 09:53:28 -0800

    Teaches will new standup time

--- plugins/standup.py
+++ plugins/standup.py
@@ -4,6 +4,6 @@ from will import settings
 
 class StandupPlugin(WillPlugin):
 
-    @periodic(hour='9', minute='0', day_of_week="mon-fri")
+    @periodic(hour='11', minute='30', day_of_week="mon-fri")
     def standup(self):
         self.say("@all Standup! %s" % settings.WILL_HANGOUT_URL)
