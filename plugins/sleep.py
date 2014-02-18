542d5a5355fe2dedfa1bb7fb8f44ad563c3fa382
Steven Skoczen <steven@quantumimagery.com>
2014-02-17 22:41:13 -0800

    Pulls phrase

--- plugins/sleep.py
+++ plugins/sleep.py
@@ -5,9 +5,9 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 class SleepPlugin(WillPlugin):
 
     @route("/sleep/fell-asleep")
-    def asleep_listener(self, phrase):
+    def asleep_listener(self):
         self.say("Steven's going to sleep")
 
     @route("/sleep/woke-up")
-    def awake_listener(self, phrase):
+    def awake_listener(self):
         self.say("G'morning, @steven!")
