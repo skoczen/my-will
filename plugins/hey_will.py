dc1122bdf2e3f3464c2026af4f1aef0f72ef0cab
Steven Skoczen <steven@quantumimagery.com>
2014-02-20 19:29:19 -0800

    try/except

--- plugins/hey_will.py
+++ plugins/hey_will.py
@@ -6,12 +6,17 @@ class HeyWillPlugin(WillPlugin):
 
     @route("/hey-will/")
     def hey_will_listener(self):
-        print self.request.__dict__
-        # print self.request.body
-        print self.request.params
-        if "phrase" in self.request.params:
-            self.say(self.request.params["phrase"])
-            return "You got it."
-        else:
-            return "Sorry, couldn't hear you!"
-        
+        try:
+            print "hey_will_listener"
+            print self
+            print self.request
+            print self.request.__dict__
+            # print self.request.body
+            print self.request.params
+            if "phrase" in self.request.params:
+                self.say(self.request.params["phrase"])
+                return "You got it."
+            else:
+                return "Sorry, couldn't hear you!"
+        except:
+            return "Sorry, something went wrong!"
