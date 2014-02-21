23952000439ecb43760b0dd11be9b47205ec1b40
Steven Skoczen <steven@quantumimagery.com>
2014-02-20 19:26:14 -0800

    more debug

--- plugins/hey_will.py
+++ plugins/hey_will.py
@@ -6,6 +6,12 @@ class HeyWillPlugin(WillPlugin):
 
     @route("/hey-will/")
     def hey_will_listener(self):
-        print self.request.body
-        self.say(self.request.body)
-        return "You got it."
+        print self.request.__dict__
+        # print self.request.body
+        print self.request.params
+        if "phrase" in self.request.params:
+            self.say(self.request.params["phrase"])
+            return "You got it."
+        else:
+            return "Sorry, couldn't hear you!"
+        
