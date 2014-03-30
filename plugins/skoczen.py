91f367aebd5b5a8d1eda4a60010af94fe1aede68
Steven Skoczen <steven@quantumimagery.com>
2014-03-29 22:25:35 -0700

    Makes it a real class.

--- plugins/skoczen.py
+++ plugins/skoczen.py
@@ -3,11 +3,11 @@ import requests
 
 class SkoczenMixin(object):
 
-    def todays_one_thing():
+    def todays_one_thing(self):
         r = requests.get("http://stevenskoczen.com/manual/one-thing/")
         return r.json()['one_thing']
 
-    def last_weigh_in():
+    def last_weigh_in(self):
         r = requests.get("http://stevenskoczen.com/manual/weights/")
         return r.json()["weights"][0]
         # {
@@ -16,6 +16,6 @@ class SkoczenMixin(object):
         #     "when": "timestamp"
         # }
 
-    def weights():
+    def weights(self):
         r = requests.get("http://stevenskoczen.com/manual/weights/")
         return r.json()
