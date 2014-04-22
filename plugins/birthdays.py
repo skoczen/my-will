5ea75875a050be92c0be4a66396f0a104b67239c
Steven Skoczen <steven@quantumimagery.com>
2014-04-21 21:09:20 -0700

    Birthday!

---
+++ plugins/birthdays.py
@@ -0,0 +1,9 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template
+
+
+class BirthdayPlugin(WillPlugin):
+
+    @periodic(month="4", day="9", minute="0", hour="0")
+    def happy_birthday_from_will(self):
+        self.say("Happy Birthday!!!! :)")
